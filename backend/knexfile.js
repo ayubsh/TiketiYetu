cdb65a2660db93eb1594a986df0bc68b749938cf
ayubsh <ibrahimayubsheikh@gmail.com>
2023-09-06 12:32:37 +0300

    backend deployment setup

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,17 +3,34 @@ require('dotenv').config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
       database: process.env.POSTGRES_DB,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-		},
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
 
-		migrations: {
-			directory: './db/migrations',
-		},
+    migrations: {
+      directory: "./db/migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.POSTGRES_URL,
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: proces.env.POSTGRES_HOST,
+    },
+
+    migrations: {
+      directory: "./db/migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 };
